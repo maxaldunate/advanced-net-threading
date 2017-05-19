c16221b15f5e229e9fa10fd0efd7a5a0390f6b92
Max Aldunate <maxaldunate@gmail.com>
2017-05-19 12:15:00 +0200

    add console net core app

---
+++ part1/ConsoleCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        private static void ThreadMethod(object state)
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            var t = new Thread(ThreadMethod);
+            t.Start("initializationdata");
+            t.Join();
+
+        }
+    }
+
+}
