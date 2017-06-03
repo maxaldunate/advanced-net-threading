7abc358288f87ea52901e6e1d701a380ecd35956
Max Aldunate <maxaldunate@gmail.com>
2017-06-03 19:10:40 +0200

    name pipes and when all

---
+++ 1-Threading/samples/Part3_NamedPipeClient.cs
@@ -0,0 +1,42 @@
+﻿/******************************************************************************
+Module:  Threading.cs
+Notices: Copyright (c) by Jeffrey Richter and Wintellect
+******************************************************************************/
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Text;
+
+public static class Part3_NamedPipeClient
+{
+    public static void Run()
+    {
+        var task = IssueClientRequestAsync("localhost", "Request");
+        Console.WriteLine(task.Result);
+    }
+
+    public static async Task<string> IssueClientRequestAsync(string serverName, string msg)
+    {
+        using (var pipe = new NamedPipeClientStream(serverName, "PipeName",
+            PipeDirection.InOut, PipeOptions.Asynchronous))
+        {
+            pipe.Connect(); //before setting read mode
+            pipe.ReadMode = PipeTransmissionMode.Message;
+
+            //Asynchronously send data to the server
+            Byte[] request = Encoding.UTF8.GetBytes(msg);
+            await pipe.WriteAsync(request, 0, request.Length);
+            // will return a Task<string> immediatly to function caller
+
+            //Asynchronously read the server's response
+            Byte[] response = new Byte[1000];
+            Int32 bytesRead = await pipe.ReadAsync(response, 0, response.Length);
+            return Encoding.UTF8.GetString(response, 0, bytesRead);
+            //** not return to function caller, instead puts in the result of Task
+        }
+    }
+}
---
+++ 1-Threading/samples/Part3_WhenAll.cs
@@ -0,0 +1,37 @@
+﻿/******************************************************************************
+Module:  Threading.cs
+Notices: Copyright (c) by Jeffrey Richter and Wintellect
+******************************************************************************/
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Text;
+
+public static class Part3_WhenAll
+{
+    public static void Run()
+    {
+        Task task = Go(100);
+        Console.WriteLine("Tasks running");
+        Console.ReadLine();
+    }
+
+    public static async Task Go(int loops)
+    {
+        Debugger.Break();
+        var requests = new List<Task<string>>(loops);
+        for (Int32 n = 0; n < requests.Capacity; n++)
+            requests.Add(Part3_NamedPipeClient.IssueClientRequestAsync("http://Wintellect.com", "Request #" + n));
+
+        string[] responses = await Task.WhenAll(requests);
+
+        Debugger.Break();
+        for (Int32 n = 0; n < responses.Length; n++)
+            Console.WriteLine(responses[n]);
+    }
+
+}
